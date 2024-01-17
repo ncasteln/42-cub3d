9bf348e71c1a866da335ac339f00fbac11899bcd
mrubina <friendy.m@gmail.com>
2024-01-17 22:00:42 +0100

    removed some unnecessary code

--- src/raycast/hooks.c
+++ src/raycast/hooks.c
@@ -6,7 +6,7 @@
 /*   By: mrubina <mrubina@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 19:01:50 by mrubina           #+#    #+#             */
-/*   Updated: 2024/01/17 20:52:30 by mrubina          ###   ########.fr       */
+/*   Updated: 2024/01/17 21:35:37 by mrubina          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,8 +35,6 @@ void	key_hook(mlx_key_data_t keydata, void *data)
 		rotateP(((t_cub3d *)data)->p, 0.1);
 	if (keydata.key == MLX_KEY_LEFT)
 		rotateP(((t_cub3d *)data)->p, -0.1);
-	mlx_image_to_window(((t_cub3d *)data)->mlx, ((t_cub3d *)data)->img, 0, 0);
-	//mlx_image_to_window(((t_cub3d *)data)->mlx, ((t_cub3d *)data)->img1, 0, 0);
 }
 
 void	mouse_hook(mlx_key_data_t keydata, int x, int y, void *param)
@@ -65,6 +63,5 @@ void draw_square(t_cub3d *data, int xStart, int yStart, int side, int color)
 
 void refresh(void *data)
 {
-	clearScreen(((t_cub3d *)data));
 	raycasting(((t_cub3d *)data));
 }
